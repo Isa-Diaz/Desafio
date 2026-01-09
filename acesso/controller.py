59832258e729983f7a1f8b7ffc6af3bf19126d12
Isabella Ventura Diaz <133471690+Isa-Diaz@users.noreply.github.com>
2026-01-09 16:21:19 -0300

    Update controller.py
    adicionando a porta 5001 para evitar conflitos com service

--- acesso/controller.py
+++ acesso/controller.py
@@ -1,7 +1,6 @@
-
 from flask import Flask, request, jsonify
-from service import criar_cliente_service, atualizar_cliente_service, calcular_score, operacao_service
-from client import listar_clientes, buscar_cliente_por_id, deletar_cliente
+from .service import criar_cliente_service, atualizar_cliente_service, calcular_score, operacao_service
+from .client import listar_clientes, buscar_cliente_por_id, deletar_cliente
 
 app = Flask(__name__)
 
@@ -9,12 +8,11 @@ app = Flask(__name__)
 def criar_cliente_controller():
     dados = request.get_json()
     result = criar_cliente_service(dados)
-   
     if type(result) is str:
         return jsonify(result), 400
     else:
         return jsonify(result), 201
-    
+
 @app.route("/clientes", methods=["GET"])
 def listar_clientes_controller():
     result = listar_clientes()
@@ -32,11 +30,11 @@ def buscar_cliente_controller(id):
 def atualizar_cliente_controller(id):
     dados = request.get_json()
     result = atualizar_cliente_service(id, dados)
-    if type(result) is str:        
+    if type(result) is str:
         return jsonify(result), 400
-    if "erro" in result:               
+    if "erro" in result:
         return jsonify(result), 404
-    return jsonify(result), 200       
+    return jsonify(result), 200
 
 @app.route("/clientes/<id>", methods=["DELETE"])
 def deletar_cliente_controller(id):
@@ -52,23 +50,23 @@ def score_controller(id):
     if "erro" in result:
         return jsonify(result), 404
     else:
-       saldo = result["saldo_cc"]
-       score = calcular_score(saldo)
-       score_resposta = {
-               "id":id,
-               "score": score
-           }    
-       return jsonify(score_resposta), 200
-    
+        saldo = result["saldo_cc"]
+        score = calcular_score(saldo)
+        score_resposta = {
+            "id": id,
+            "score": score
+        }
+        return jsonify(score_resposta), 200
+
 @app.route("/clientes/<id>/operacao", methods=["POST"])
 def operacao_controller(id):
     dados = request.get_json()
     result = operacao_service(id, dados)
-
     if type(result) is str:
         return jsonify({"erro": result}), 400
-
     if "erro" in result:
         return jsonify(result), 404
-
     return jsonify(result), 200
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
